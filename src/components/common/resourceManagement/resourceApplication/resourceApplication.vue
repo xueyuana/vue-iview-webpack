c519c2cae1fd2a7a05fdf3aba19cded77f9622d4
李秀芬 <lixiufen@syswin.com>
2017-05-23 16:11:53 +0800

    提交测试3

--- src/components/common/resourceManagement/resourceApplication/resourceApplication.vue
+++ src/components/common/resourceManagement/resourceApplication/resourceApplication.vue
@@ -246,6 +246,7 @@
                 domain: "",
                 env: "develop",
                 application_status: "待提交",
+                approval_status:"流程不存在",
                 resource_list: [
                     {
                         res_name: "",
@@ -342,11 +343,13 @@
             }
         })*/
             let self=this;
-            let newjson=JSON.stringify(self.formInline);
+
             const url=common.apihost+'resource/';
             //判断是否是第一次提交第一次用post，第二次用put
            if(self.formInline.res_id){
                let newUrl=url+self.formInline.res_id;
+               self.formInline.approval_status="审批中"
+               let newjson=JSON.stringify(self.formInline);
                this.$http.put(newUrl,newjson ).then(function (response) {
 
                    console.log(response);
