7b6370afb25787ea03c4a725c6fb8e110a2ef488
muhongying <609818552@qq.com>
2017-05-24 22:42:37 +0800

    添加资源视图2

--- src/components/common/workbench/resourceView/resourceView.vue
+++ src/components/common/workbench/resourceView/resourceView.vue
@@ -1,28 +1,34 @@
 <template>
     <div>
-      <div class="view-left">
-          <Menu :theme="light" active-name="1">
-              <Menu-item name="1">
-                  <Icon type="document-text"></Icon>
-                  文章管理
-              </Menu-item>
-              <Menu-item name="2">
-                  <Icon type="chatbubbles"></Icon>
-                  评论管理
-              </Menu-item>
-              <Menu-item name="3">
-                  <Icon type="heart"></Icon>
-                  用户留存
-              </Menu-item>
-              <Menu-item name="4">
-                  <Icon type="heart-broken"></Icon>
-                  流失用户
-              </Menu-item>
-          </Menu>
-      </div>
-        <div class="view-right">
+        <Row>
+            <Col span="6">
+              <div class="view-left">
+                <Menu :theme="light" active-name="1" width="auto">
+                    <Menu-item name="1">
+                        <Icon type="document-text"></Icon>
+                        文章管理
+                    </Menu-item>
+                    <Menu-item name="2">
+                        <Icon type="chatbubbles"></Icon>
+                        评论管理
+                    </Menu-item>
+                    <Menu-item name="3">
+                        <Icon type="heart"></Icon>
+                        用户留存
+                    </Menu-item>
+                    <Menu-item name="4">
+                        <Icon type="heart-broken"></Icon>
+                        流失用户
+                    </Menu-item>
+                </Menu>
+            </div>
+            </Col>
+            <Col span="18">
+                <div class="view-right">
 
-        </div>
+                </div>
+            </Col>
+        </Row>
     </div>
 </template>
 
