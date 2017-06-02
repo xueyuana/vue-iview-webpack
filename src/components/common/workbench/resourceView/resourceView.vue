5a9661acc8ab9e6af4911b03581c1d8f2a3e2b22
muhongying <609818552@qq.com>
2017-06-02 11:45:18 +0800

    完成视图的基本配置

--- src/components/common/workbench/resourceView/resourceView.vue
+++ src/components/common/workbench/resourceView/resourceView.vue
@@ -4,10 +4,22 @@
             <Col span="4">
               <div class="view-left">
                 <Menu :theme="theme" active-name="1" width="auto" @on-select="goResourceTree">
-                    <Menu-item :name="data.item_id" v-for="(data,index) in datas" :key="data">
-                        <Icon type="star"></Icon>
-                        {{ data.item_name }}
-                    </Menu-item>
+                    <Submenu :name="menuData.item_id" v-for="(menuData,index) in menuDatas" :key="menuData">
+                        <template slot="title">
+                           {{menuData.item_name}}
+                        </template>
+                        <Menu-item :name="0">
+                            {{1}}
+                        </Menu-item>
+                        <!--<Menu-item :name="instanceObj.id" v-for="instanceObj in menuData.instance" :key="instanceObj">-->
+                            <!--{{ instanceObj.resource }}-->
+                        <!--</Menu-item>-->
+
+                    </Submenu>
+                    <!--<Menu-item :name="data.item_id" v-for="(data,index) in menuDatas" :key="data">-->
+                        <!--<Icon type="star"></Icon>-->
+                        <!--{{ data.item_name }}-->
+                    <!--</Menu-item>-->
                 </Menu>
             </div>
             </Col>
@@ -30,7 +42,7 @@
         data () {
             return {
                 theme: 'light',
-                datas:[]
+                menuDatas:[]
             }
         },
         mounted() {
@@ -53,13 +65,35 @@
                                 console.log(response);
                                 if (response.body.code === 200) { // 请求成功
                                     let backDatas = response.body.result.res;
-                                    this.datas=backDatas;
+//                                    this.menuDatas=backDatas;
+                                    return backDatas;
                                 }
+                        }).then(menuDatas=>{
+                            // 根据部署单元名称查询所有的部署实例
+                           for(let i=0;i<menuDatas.length;i++) {
+                               let menuDataObj=menuDatas[i];
+                               let urlInstance=common.apihost+'resource/?project='+menuDataObj.item_name;
+                               this.$http.get(urlInstance)
+                                       .then(response=>{
+                                           if( response.body.code===200){
+                                               // 给menuDatas添加实例属性
+                                                 menuDataObj.instance=response.body.result.msg;
+//
+                                           }
+                                       })
+                           }
+                          this.menuDatas=menuDatas;
+                          console.log(this.menuDatas);
+//                          console.log(this.submenuDatas);
                         });
             },
             // 跳转到相应的资源架构图
             goResourceTree (name) {
                 console.log(name);
+                // 根据id获取部署实例的架构数据
+
+                // 将数据保存到状态池中
+
                 this.$router.push({ name:"resourceTree",params:{ resource_id:name}})
             }
         }
