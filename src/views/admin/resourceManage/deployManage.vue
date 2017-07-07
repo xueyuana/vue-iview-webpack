497e4d995db00cd9c22260dfafa2552cf2f96c7c
zhujiejie <851930606@qq.com>
2017-07-07 10:48:51 +0800

    添加公共类overflow

--- src/views/admin/resourceManage/deployManage.vue
+++ src/views/admin/resourceManage/deployManage.vue
@@ -47,27 +47,31 @@
     <Modal
         title="分配公网IP"
         width="300"
-        v-model="option"
+        v-model="visible"
         @on-ok="onOk"
         :mask-closable="false"
         class-name="vertical-center-modal">
       <div class="from_wrap">
-        <Form ref="formCustom" :model="ipForm"  :label-width="80">
-          <Form-item label="IP类型:">
+        <Form ref="formCustom" :model="ipForm" :rules="ruleValidate" :label-width="90">
+          <Form-item label="IP类型:" prop="ip_type">
             <Select v-model="ipForm.ip_type" placeholder="请选择" @on-change="onSelectType" clearable>
               <Option value="online">上线IP</Option>
               <Option value="test">测试IP</Option>
             </Select>
           </Form-item>
-          <Form-item label="公网IP地址:">
+          <Form-item label="公网IP地址:" prop="ip_uuid">
             <Select v-model="ipForm.ip_uuid" :placeholder="pubIpList.length ? '请选择' : '空'" filterable clearable>
               <Option v-for="(item, index) in pubIpList" :value="item.uuid" :key="index">{{item.ip}}</Option>
             </Select>
           </Form-item>
-          <Form-item label="内网IP地址:">
-            <Input v-model="ipForm.inner_ip" placeholder="192.168.2.1/24"></Input>
+          <Form-item label="内网IP地址:" prop="internal_ip">
+            <Input v-model="ipForm.internal_ip" placeholder="192.168.2.1/24"></Input>
           </Form-item>
         </Form>
+      </div>
+      <div slot="footer">
+        <Button type="text" @click.native="onCancel">取消</Button>
+        <Button type="primary" @click.native="onOk">确定</Button>
       </div>
     </Modal>
 
@@ -96,6 +100,18 @@
 
   export default {
     data() {
+      const validateIp = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入IP'));
+        } else {
+          if (!this.ipReg.test(value)) {
+            callback(new Error('请输入正确IP地址'));
+          } else {
+            callback();
+          }
+        }
+      }
+
       return {
         formItem: {
           date: [],
@@ -112,9 +128,9 @@
         columns: [
           {
             title: '序号',
-            type: 'index',
+            key: 'index',
             align: 'center',
-            width: '100'
+            width: '60'
           },
           {
             title: '部署实例名称',
@@ -138,7 +154,7 @@
           },
           {
             title: '内网IP',
-            key: 'inner_ip',
+            key: 'internal_ip',
             align: 'center'
           },
           {
@@ -161,7 +177,10 @@
                 },
                 on: {
                   click: () => {
-                    this.option = true
+                    this.$refs['formCustom'].resetFields()
+                    this.visible = true
+                    this.ipForm.instance_id = params.row.instance_id
+                    this.index = params.index
                   }
                 }
               }, '发布')
@@ -173,33 +192,44 @@
         filterDate: [],
 
         ipForm: {
+          instance_id: '',
           ip_type: '',
           ip_uuid: '',
-          inner_ip: ''
+          internal_ip: ''
         },
-        ipTypeList: [],
-        pubIpList: [],
-        option: false,
+        ruleValidate: {
+          ip_type: [
+            { required: true, message: '请选择IP类型', trigger: 'change' }
+          ],
+          ip_uuid: [
+            { required: true, message: '请选择公网IP', trigger: 'change' }
+          ],
+          internal_ip: [
+            { required: true, validator: validateIp, trigger: 'change' }
+          ]
+        },
+        pubIpList: [],  // 公网IP列表
+        visible: false,
+        index: '',      // 记录修改的数据
+        ipReg:/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/,
+
+
         pageSize: 10,
         num: 1
       }
     },
 
     created() {
+        // 1. 部署实例列表
       this.onInquire()
-        // 1. 实例名称
+        // 2. 实例名称
       this.getInstance().then(res => {
-        this.instanceList = res.body.result.res
+        this.instanceList = res
       })
-    },
-
-    watch: {
-
     },
 
     methods: {
 
-        // 2. 部署实例列表
       onInquire() {
         let query = {}
         this.formItem.date[0] && (query.start_time = formatDate(this.formItem.date[0]))
@@ -208,7 +238,11 @@
         this.formItem.instance_name && (query.instance_name = this.formItem.instance_name)
 
         this.getInstance(query).then(res => {
-          this.filterDate = this.mockTableData(res.body.result.res, this.pageSize, 1)
+          console.log('部署实例列表',  res)
+          res.forEach((item, index) => {
+            item.index = index + 1
+          })
+          this.filterDate = this.mockTableData(res, this.pageSize, 1)
         })
       },
       handleReset(name) {
@@ -229,18 +263,40 @@
 
         // 4. 确定分配
       onOk() {
-        let body = {}
-        body.
-
-        this.$http.post('api/ip_manager/ip_publish', body).then(res => {
-          if (res.body.code === 200) {
-
+        this.$refs['formCustom'].validate((valid) => {
+          if (valid) {
+            this.$http.post('api/ip_manager/ip_publish', this.ipForm).then(res => {
+              if (res.body.code === 200) {
+                this.updateTable()
+                this.visible = true
+              } else {
+                this.$Message.error(res.body.result.msg)
+              }
+            }, err => {
+              console.log(err)
+            })
           } else {
-            this.$Message.error(res.body.result.msg)
+            this.$Message.error('表单验证失败!')
+            return
           }
-        }, err => {
-          console.log(err)
         })
+      },
+
+      onCancel() {
+        this.visible = false
+      },
+
+        // 5. 更新列表
+      updateTable() {
+        let obj = this.pubIpList.filter(item => {
+          return item.uuid === this.ipForm.ip_uuid
+        })
+        if (this.ipForm.ip_type === 'online') {
+          this.filterDate[this.index].online_ip = obj[0].ip
+        } else {
+          this.filterDate[this.index].test_ip = obj[0].ip
+        }
+        this.filterDate[this.index].internal_ip = this.ipForm.internal_ip
       },
 
       getInstance(obj) {    // 获取实例列表
@@ -249,7 +305,7 @@
             params: obj || {}
           }).then(res => {
             if (res.body.code === 200) {
-              resolve(res)
+              resolve(res.body.result.res)
             } else {
               this.$Message.error(res.body.result.msg)
             }
